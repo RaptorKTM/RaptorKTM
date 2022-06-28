1283fcc7911bfde416c72cd331c247fe3182cd88
RaptorKTM <105537220+RaptorKTM@users.noreply.github.com>
2022-06-28 16:25:31 +0200

    Update compteur.js

--- js/compteur.js
+++ js/compteur.js
@@ -2,7 +2,7 @@ const counter = document.getElementById('counter')
 
 
 const updateCounter = async () => {
-  const data = await fetch('https://api.countapi.xyz/update/raptorktm-counter/visits')
+  const data = await fetch('https://api.countapi.xyz/hit/raptorktm-counter/visits')
   const count = await data.json()
   counter.innerHTML = count.value
   counter.style.filter = 'blur(O)'
